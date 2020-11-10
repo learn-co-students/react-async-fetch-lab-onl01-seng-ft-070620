abf9f5b7bb88c268f290cc70640bdc5b772d0748
Directrix777 <andrew.j.ribas@gmail.com>
2020-11-10 11:47:31 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,44 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component
+{
+    // constructor(props)
+    // {
+    //     super()
+    //     this.state = {}
+    // }
+
+    // changeSomethingOnDeeperLevelOfState()
+    // {
+    //     this.setState({
+    //         keyOfDeeperLevel: {
+    //           ...this.state.keyOfDeeperLevel,
+    //           thingWeWannaChange: 'New Value'
+    //         }
+    //       });
+    // }
+
+    // changeStateDynamically()
+    // {
+    //     this.setState((previousState => {return {value: previousState.value + 1}})) 
+    //     //Just adding one as an example. You could call a function on this data if you wanted.
+    //     //previousState parameter is automatically passed in. Name is descriptive.
+    // }
+
+    componentDidMount() {
+        //this.interval = setInterval(this.fetchWeather, 15000);
+        //can set up a constant fetch
+        fetch('http://api.open-notify.org/astros.json')
+    }
+
+    // componentWillUnmount() {
+    //     //clearInterval(this.interval);
+    //     //would stop the fetch set up in componentDidMount
+    // }
+
+    render()
+    {
+        return (<div>This is a wireframe from the Workshop</div>)
+    }
+}
